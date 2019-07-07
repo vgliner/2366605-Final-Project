73153c1ddb44660c3e97f6ee88da907901b30c6e
Vadim <vadim.gliner@gmail.com>
2019-07-08 01:02:01 +0300

    Modified DB path

--- Main_ECG_renderer_example.py
+++ Main_ECG_renderer_example.py
@@ -3,7 +3,7 @@ import cv2
 from ECG_renderer import *
 from ECG_multi_lead_dataloader import *
 
-ECG_test=ECG_Multilead_Dataset(root_dir=os.getcwd()+'\\Chineese_database\\',transform= None, partial_upload=True) 
+ECG_test=ECG_Multilead_Dataset(root_dir=r'C:\Users\vgliner\OneDrive - JNJ\Desktop\Data'+'\\',transform= None, partial_upload=True) 
 
 draw_ECG_multilead_vanilla(ECG_test[2][0])
 print('Finished')
