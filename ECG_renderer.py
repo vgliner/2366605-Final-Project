af22544ee73f34852236cbcdb7694638924dfd35
Vadim <vadim.gliner@gmail.com>
2019-07-10 23:28:41 +0300

    Calibration signal added

--- ECG_renderer.py
+++ ECG_renderer.py
@@ -11,10 +11,8 @@ def draw_ECG_multilead_vanilla(ECG_info: tuple):
              'aVF', 'V1', 'V2', 'V3', 'V4', 'V5', 'V6']
     
     img = plt.imread("640px-ECG_paper_12_leads.svg.png")
-    #img=scipy.ndimage.zoom(img, 10, order=0)
-    # fig, ax = plt.subplots()
-    # ax.imshow(img)
-    #plt.show()
+    pixels_to_mV= 150//10 # 147 pixels /10mV
+    pixels_to_sec= (384.76-93.6)//(0.2*3) # 
     image_pixels_ratio=[6000,2500] # (X,Y)
     fig, ax = plt.subplots()
     y_scaling_factor=image_pixels_ratio[1]//6
@@ -22,21 +20,18 @@ def draw_ECG_multilead_vanilla(ECG_info: tuple):
     ax.plot(ECG_info[1]*y_scaling_factor,  linewidth=1, color='black')
     B=ECG_info[0]
     cntr_trans=[0,3,6,9,1,4,7,10,2,5,8,11]
-    for cntr in range(4):
-        ax.plot(np.linspace(cntr*image_pixels_ratio[0]//5,cntr*image_pixels_ratio[0]//5+len(B[cntr_trans[cntr], :]),num=len(B[cntr_trans[cntr], :])),B[cntr_trans[cntr], :]*y_scaling_factor+4/5*image_pixels_ratio[1],  linewidth=1, color='black')
+    for extrnl_cntr in range(3):
+        # Plot a calibration signal
+        x_cal_signal=np.linspace(0,0.2*pixels_to_sec,num=np.floor(0.2*pixels_to_sec))
+        y_cal_signal=np.ones_like(x_cal_signal)*pixels_to_mV*10+B[cntr_trans[extrnl_cntr*4]][0]*y_scaling_factor+(4-extrnl_cntr)/5*image_pixels_ratio[1]-extrnl_cntr/10*image_pixels_ratio[1]
+        y_cal_signal[-2:]=0+B[cntr_trans[extrnl_cntr*4]][0]*y_scaling_factor+(4-extrnl_cntr)/5*image_pixels_ratio[1]-extrnl_cntr/10*image_pixels_ratio[1]
+        ax.plot(x_cal_signal,y_cal_signal,linewidth=1, color='black')        
+        for cntr in range(4):
+            ax.plot(np.linspace(cntr*image_pixels_ratio[0]//4.7+len(y_cal_signal),cntr*image_pixels_ratio[0]//4.7+len(B[cntr_trans[cntr], :])+len(y_cal_signal),
+            num=len(B[cntr_trans[cntr], :])),B[cntr_trans[extrnl_cntr*4+cntr], :]*y_scaling_factor+(4-extrnl_cntr)/5*image_pixels_ratio[1]-extrnl_cntr/10*image_pixels_ratio[1],
+            linewidth=1, color='black')
 
     ax.axis('off')
 
     plt.show()
 
-    fig, axes = plt.subplots(nrows=3, ncols=4)
-    titles = ['Lead1', 'Lead2', 'Lead3', 'aVR', 'aVL','aVF', 'V1', 'V2', 'V3', 'V4', 'V5', 'V6']
-    B=ECG_info[0]
-    for ax, cntr in zip(axes.flatten(), range(12)):
-        ax.plot(B[cntr_trans[cntr], :], linewidth=2.0,color='black')
-        ax.set(title=titles[cntr_trans[cntr]])
-        ax.axis('off')
-
-    plt.plot()
-    plt.show()
-    print('Here')
